b67ba19b44d37273001281b6b8fd05572e0e3a8b
Cristian <cristiand.vargas@udea.edu.co>
2020-09-22 18:16:10 -0500

    Se crearon archivos de wzip

---
+++ enunciado/wzip/wzip.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void compressFile(char *fname);
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        compressFile(argv[i]);
+    }
+
+    return 0;
+}
+
+void compressFile(char *fname)
+{
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+    FILE *inFile = fopen(fname, "r");
+    if (inFile == NULL)
+    {
+        printf("wzip: cannot open file\n");
+        exit(1);
+    }
+    while ((read = getline(&line, &len, inFile)) != -1)
+    {
+
+        char aux = line[0];
+        int count = 1;
+        for (int i = 1; i < len; i++)
+        {
+            if (aux == line[i]) {
+                count++;
+            } else {
+                fwrite(&count, 4, 1, stdout);
+                fwrite(&aux, 1, 1, stdout);
+                aux = line[i];
+                count = 1;
+            }
+        }
+    }
+    fclose(inFile);
+}
+
+
+

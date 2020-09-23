3b1154c590187af8c1e839e7816db3009e3349b0
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-09-23 18:36:02 -0500

    Prueba de codigo anterior

--- enunciado/wzip/wzip.c
+++ enunciado/wzip/wzip.c
@@ -17,39 +17,67 @@ int main(int argc, char *argv[])
     //aaaaaaaaaaa
     for (int i = 1; i < argc; i++)
     {
-
+        char *line = NULL;
+        size_t len = 0;
+        size_t read;
         FILE *inFile = fopen(argv[i], "r");
         if (inFile == NULL)
         {
             printf("wzip: cannot open file\n");
             exit(1);
         }
-
-        while ((currChar = fgetc(inFile)) != EOF)
+        while ((read = getline(&line, &len, inFile)) != -1)
         {
-            if (prevChar != currChar && prevChar == EOF)
+
+            char aux = line[0];
+            int count = 1;
+            for (int i = 1; i < len; i++)
             {
-                prevChar = currChar;
-                count = 1;
-            }
-            else if (prevChar == currChar)
-            {
-                count++;
-            }
-            else if (prevChar != currChar)
-            {
-                puts("entre aqui");
-                // fwrite(&count, 4, 1, stdout);
-                // fwrite(&prevChar, 1, 1, stdout);
-                prevChar = currChar;
-                count = 1;
+                if (aux == line[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    fwrite(&count, 4, 1, stdout);
+                    fwrite(&aux, 1, 1, stdout);
+                    aux = line[i];
+                    count = 1;
+                }
             }
         }
-
         fclose(inFile);
-    }
+        //     if (inFile == NULL)
+        //     {
+        //         printf("wzip: cannot open file\n");
+        //         exit(1);
+        //     }
 
-    fwrite(&count, 1, 4, stdout);
-    //fwrite(&prevChar, 1, 1, stdout);
-    return 0;
-}
+        //     while ((currChar = fgetc(inFile)) != EOF)
+        //     {
+        //         if (prevChar != currChar && prevChar == EOF)
+        //         {
+        //             prevChar = currChar;
+        //             count = 1;
+        //         }
+        //         else if (prevChar == currChar)
+        //         {
+        //             count++;
+        //         }
+        //         else if (prevChar != currChar)
+        //         {
+        //             puts("entre aqui");
+        //             // fwrite(&count, 4, 1, stdout);
+        //             // fwrite(&prevChar, 1, 1, stdout);
+        //             prevChar = currChar;
+        //             count = 1;
+        //         }
+        //     }
+
+        //     fclose(inFile);
+        // }
+
+        // fwrite(&count, 1, 4, stdout);
+        // //fwrite(&prevChar, 1, 1, stdout);
+        // return 0;
+    }

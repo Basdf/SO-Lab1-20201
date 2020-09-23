e35a7e705846bc46c70a216c22f4ab876150d6d7
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-09-23 18:37:15 -0500

    cambiando char por int en count

--- enunciado/wzip/wzip.c
+++ enunciado/wzip/wzip.c
@@ -11,73 +11,45 @@ int main(int argc, char *argv[])
     }
     int currChar;        /* current characters */
     int prevChar;        /* previous characters */
-    unsigned char count; /* number of characters in a run */
+    unsigned int count; /* number of characters in a run */
     prevChar = EOF;      /* force next char to be different */
     count = 0;
     //aaaaaaaaaaa
     for (int i = 1; i < argc; i++)
     {
-        char *line = NULL;
-        size_t len = 0;
-        size_t read;
+
         FILE *inFile = fopen(argv[i], "r");
         if (inFile == NULL)
         {
             printf("wzip: cannot open file\n");
             exit(1);
         }
-        while ((read = getline(&line, &len, inFile)) != -1)
-        {
 
-            char aux = line[0];
-            int count = 1;
-            for (int i = 1; i < len; i++)
+        while ((currChar = fgetc(inFile)) != EOF)
+        {
+            if (prevChar != currChar && prevChar == EOF)
             {
-                if (aux == line[i])
-                {
-                    count++;
-                }
-                else
-                {
-                    fwrite(&count, 4, 1, stdout);
-                    fwrite(&aux, 1, 1, stdout);
-                    aux = line[i];
-                    count = 1;
-                }
+                prevChar = currChar;
+                count = 1;
+            }
+            else if (prevChar == currChar)
+            {
+                count++;
+            }
+            else if (prevChar != currChar)
+            {
+                puts("entre aqui");
+                // fwrite(&count, 4, 1, stdout);
+                // fwrite(&prevChar, 1, 1, stdout);
+                prevChar = currChar;
+                count = 1;
             }
         }
+
         fclose(inFile);
-        //     if (inFile == NULL)
-        //     {
-        //         printf("wzip: cannot open file\n");
-        //         exit(1);
-        //     }
-
-        //     while ((currChar = fgetc(inFile)) != EOF)
-        //     {
-        //         if (prevChar != currChar && prevChar == EOF)
-        //         {
-        //             prevChar = currChar;
-        //             count = 1;
-        //         }
-        //         else if (prevChar == currChar)
-        //         {
-        //             count++;
-        //         }
-        //         else if (prevChar != currChar)
-        //         {
-        //             puts("entre aqui");
-        //             // fwrite(&count, 4, 1, stdout);
-        //             // fwrite(&prevChar, 1, 1, stdout);
-        //             prevChar = currChar;
-        //             count = 1;
-        //         }
-        //     }
-
-        //     fclose(inFile);
-        // }
-
-        // fwrite(&count, 1, 4, stdout);
-        // //fwrite(&prevChar, 1, 1, stdout);
-        // return 0;
     }
+
+    fwrite(&count, 1, 4, stdout);
+    //fwrite(&prevChar, 1, 1, stdout);
+    return 0;
+}

88b476aea388af153798bc9d241d90b4dd2e460a
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-09-23 18:04:35 -0500

    Prueba con fgetc #2

--- enunciado/wzip/wzip.c
+++ enunciado/wzip/wzip.c
@@ -9,7 +9,11 @@ int main(int argc, char *argv[])
         printf("wzip: file1 [file2 ...]\n");
         exit(1);
     }
-
+    int currChar;        /* current characters */
+    int prevChar;        /* previous characters */
+    unsigned char count; /* number of characters in a run */
+    prevChar = EOF;      /* force next char to be different */
+    count = 0;
     for (int i = 1; i < argc; i++)
     {
 
@@ -19,68 +23,30 @@ int main(int argc, char *argv[])
             printf("wzip: cannot open file\n");
             exit(1);
         }
-        int currChar;        /* current characters */
-        // int prevChar;        /* previous characters */
-        // unsigned char count; /* number of characters in a run */
 
-        // /* encode inFile */
-        // prevChar = EOF; /* force next char to be different */
-        // count = 0;
-        // aaaab
-        /* read input until there's nothing left */
         while ((currChar = fgetc(inFile)) != EOF)
         {
-            printf("%c",currChar);
-            // fputc(currChar, outFile);
-
-            // /* check for run */
-            // if (currChar == prevChar)
-            // {
-            //     /* we have a run.  count run length */
-            //     count = 0;
-
-            //     while ((currChar = fgetc(inFile)) != EOF)
-            //     {
-            //         if (currChar == prevChar)
-            //         {
-            //             count++;
-
-            //             if (count == UCHAR_MAX)
-            //             {
-            //                 /* count is as long as it can get */
-            //                 fputc(count, outFile);
-
-            //                 /* force next char to be different */
-            //                 prevChar = EOF;
-            //                 break;
-            //             }
-            //         }
-            //         else
-            //         {
-            //             /* run ended */
-            //             fputc(count, outFile);
-            //             fputc(currChar, outFile);
-            //             prevChar = currChar;
-            //             break;
-            //         }
-            //     }
-            // }
-            // else
-            // {
-            //     /* no run */
-            //     prevChar = currChar;
-            // }
-
-            // if (currChar == EOF)
-            // {
-            //     /* run ended because of EOF */
-            //     fputc(count, outFile);
-            //     break;
-            // }
+            if (prevChar != currChar && prevChar == EOF)
+            {
+                prevChar = currChar;
+                count = 1;
+            }
+            if (prevChar == currChar)
+            {
+                count++;
+            }
+            if (prevChar != currChar)
+            {
+                fwrite(&count, 4, 1, stdout);
+                fwrite(&prevChar, 1, 1, stdout);
+                prevChar = currChar;
+                count = 1;
+            }
         }
 
         fclose(inFile);
     }
-
+    fwrite(&count, 4, 1, stdout);
+    fwrite(&prevChar, 1, 1, stdout);
     return 0;
 }
